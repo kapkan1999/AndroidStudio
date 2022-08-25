80160c2973b9ddf5cf81f956015a28c8efad1ec2
Michael Terehin <kapkan1999@list.ru>
2022-08-25 23:01:06 +0700

    Ошибка

--- app/src/main/java/com/example/myapplication/Ac3.java
+++ app/src/main/java/com/example/myapplication/Ac3.java
@@ -10,9 +10,9 @@ public class Ac3 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView textView = new TextView(this);
-        textView.setTextSize(26);
-        textView.setPadding(16, 120, 16, 16);
+        TextView textView = findViewById(R.id.textView2);
+        //textView.setTextSize(26);
+       // textView.setPadding(16, 120, 16, 16);
 
         Bundle arguments = getIntent().getExtras();
 
