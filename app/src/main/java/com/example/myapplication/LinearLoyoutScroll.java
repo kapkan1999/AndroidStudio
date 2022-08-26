758ac624e520c7dbec3e827056e48f85db356474
Michael Terehin <kapkan1999@list.ru>
2022-08-26 16:50:03 +0700

    Добавил activity_scroll.xml

---
+++ app/src/main/java/com/example/myapplication/LinearLoyoutScroll.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinearLoyoutScroll extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_loyout_scroll);
+    }
+}
